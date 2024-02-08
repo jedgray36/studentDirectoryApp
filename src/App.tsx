1a6da8fa1ab18e30ce1e3eb9020a89caf93b2bc6
Jed Gray <62748148+jedgray36@users.noreply.github.com>
2024-02-08 10:34:22 -0500

    student directory

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+
+
+interface Student {
+  firstName: string,
+  lastName: string,
+  age: number,
+  subjects: string[]
+}
+
 
 function App() {
+
+const [students, setStudents] = useState<Student[]>([]);
+const api = "";
+
+const getStudents = async () => {
+const studentData = await axios.get(api);
+setStudents(studentData.data)
+}
+
+
+const addStudents = () => {
+
+}
+
+const updateStudent = (id: any) => {
+
+}
+
+
+const deleteStudent = (id: any) => { 
+
+}
+
+const getStudent = (id: any) => {
+
+}
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Student Directory</h1>
+
+      <div className='students'>Students</div>
+      <ul>
+        {students.map((student) => 
+          <li>
+            <span>{student.firstName + " " + student.lastName}</span>
+            <span>{student.age}</span>
+            <span>{student.subjects}</span>
+          </li>
+        )}
+      </ul>
     </div>
   );
 }
